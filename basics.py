8ed5bb847f5cc135e7da754933c82a1c1eb7da1d
Jeremy Thoveron <jeremy.thoveron-@epitech.eu>
2021-03-20 20:31:00 +0100

    fonctions pour le salaire & l'enregistrement des membres

--- basics.py
+++ basics.py
@@ -1,18 +1,25 @@
 import discord
 import db
+from datetime import date
 
 #!salary
     # create adding once a month point func check role si admin donne a tout le monde
     # check pay date to ensure no multiple pay
     # 7500 comissaire, 2500 garde, 2500 sommelier, 15000 tcheka
-def salary(message):
+
+def salary(cur, message):
+    today = date.today()
     admin = False
     str = ''
     for role in (message.author.roles):
         if (role.name == 'Président Général' or role.name == 'Commissariat du peuple'):
             admin = True
     if admin:
-           str = db.give_points(message)
+        data = db.get_salaries(cur)
+        for user in data:
+            if (user[1] != today.month):
+                db.add_points(cur, user[0], user[2])
+                db.update_month(cur, user[0], today.month)
     else:
         str = "You don't have the rights to use this command"
     return str
@@ -25,8 +32,32 @@ def show_francs(cur, message):
 #!register
     # create account & give 200 points check role si commisaire ou président
 def register_members(cur, message):
-    str = register_members_in_db(cur, message)
-    
+    """data = message.content.split('/')
+    for name in data:
+        if (name.starstwith('!') or not name):
+            pass
+        else:
+            toto = messsage.guild.members
+            for member in toto:
+                if member.name =
+            #get discord member to register id"""
+
+def register_roles(cur, message):
+    for role in message.guild.roles:
+        if (role.name == 'Tchéka'):
+            salary = 15000
+        elif (role.name == 'Commissariat du peuple'):
+            salary = 7500
+        elif (role.name == "Sommelier de l'élysée"):
+            salary = 2500
+        elif (role.name == "Garde du corp"):
+            salary = 2500
+        elif (role.name == "Peuple Souverain"):
+            salary = 500
+        else:
+            salary = 0
+        db.register_roles(cur, (salary, role.id))
+
     #!transfer
     # transfer money from one account to the other
     # Peuple ne peut pas donner d'argent aux immigrés & aux ennemis du peuple
--- db.py
+++ db.py
@@ -5,8 +5,14 @@ def connect_db():
     cur = con.cursor()
     return cur
 
-def give_points(cur, message):
-    return ('Not Yet implemented')
+def get_salaries(cur):
+    cur.execute("SELECT c.discord_id, c.date_paye, r.salary FROM comptes c INNER JOIN roles r on c.role_id = r.discord_role_id")
+    res = cur.fetchall()
+    return res
+
+def update_month(cur, discord_id, month):
+    param = (month, discord_id)
+    cur.execute("UPDATE comptes SET date_paye = ? WHERE discord_id = ?", param)
 
 def check_account(cur, message):
     cur.execute("SELECT francs FROM comptes WHERE pseudo = ?", message.author.name)
@@ -14,21 +20,24 @@ def check_account(cur, message):
     str = 'Your account value is ' + res[0]
     return str
 
+def register_roles(cur, message, val):
+    cur.execute("INSERT INTO roles (salary, discord_role_id) VALUES ?", val)
+
 def register_members(cur, message):
     return ('Not yet implemented')
 
-def add_points(cur, pseudo, points):
-    cur.execute("SELECT francs FROM comptes WHERE pseudo = ?", pseudo)
+def add_points(cur, discord_id, points):
+    cur.execute("SELECT francs FROM comptes WHERE discord_id = ?", discord_id)
     res = cur.fetchone()
     val = res[0]
     val += points
-    param = (val, pseudo)
-    cur.execute("UPDATE comptes SET francs = ? WHERE pseudo =?", param)
+    param = (val, discord_id)
+    cur.execute("UPDATE comptes SET francs = ? WHERE discord_id =?", param)
 
-def decrease_points(cur, pseudo, points):
-    cur.execute("SELECT francs FROM comptes WHERE pseudo = ?", pseudo)
+def decrease_points(cur, discord_id, points):
+    cur.execute("SELECT francs FROM comptes WHERE discord_id = ?", discord_id)
     res = cur.fetchone()
     val = res[0]
     val -= points
-    param = (val, pseudo)
-    cur.execute("UPDATE comptes SET francs = ? WHERE pseudo =?", param)
+    param = (val, discord_id)
+    cur.execute("UPDATE comptes SET francs = ? WHERE discord_id =?", param)
