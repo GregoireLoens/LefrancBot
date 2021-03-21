25932fd4445c53d847274081ce61c46cb083f34f
gloens <gregoire.loens59670@gmail.com>
2021-03-21 10:29:55 +0100

    add acount id in gamblers

--- gambler.py
+++ gambler.py
@@ -1,14 +1,14 @@
 class Gambler():
     _table = "gamblers"
 
-    def __init__(self, user_id, bet_id)
+    def __init__(self, account_id, bet_id)
         con = sqlite3.connect('../../db/franc.db')
         self._cursor = con.cursor()
-        self._user_id = user_id
+        self._account_id = account_id
         self._bet_id = bet_id
         self._exist = False
         try:
-            self.cursor.execute("SELECT bet_amount, choice from ? WHERE user_id = ? AND bet_id = ?", self._table, self.user_id, self.bet_id)
+            self._cursor.execute("SELECT bet_amount, choice from ? WHERE account_id = ? AND bet_id = ?", self._table, self.user_id, self.bet_id)
             self._bet_amount = self._cursor.fetchone[0]
             self._choice = self._cursor.fetchone[1]
             self._exist = True
@@ -23,7 +23,7 @@ class Gambler():
         self._bet_amount
 
     def create(self, bet_amount, choice):
-        self.cursor.execute("INSERT INTO ? (user_id, bet_id, bet_amount, choice) VALUES (?, ?, ?, ?);", self._table, self.user_id, self.bet_id, bet_ammount, choice)
+        self.cursor.execute("INSERT INTO ? (account_id, bet_id, bet_amount, choice) VALUES (?, ?, ?, ?);", self._table, self.user_id, self.bet_id, bet_ammount, choice)
         self.bet_amount = bet_amount
         self.choice = choice
 
