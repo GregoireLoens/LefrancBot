e06a0ad3ee6a20a9792cfadc0f1d9134522d9b26
Hugo Soszynski <hugo.soszynski@outlook.com>
2021-03-28 22:58:21 +0200

    ADDED PREVENT CONCURRENCY DECORATOR TO ALL MODELS

--- models/Gambler.py
+++ models/Gambler.py
@@ -1,9 +1,16 @@
 import sqlite3
+from threading import Lock
+from toolkit.db import prevent_concurrent
 from models.Model import Model
+
+
+# Lock used to prevent conccurent acces to the same table into the database
+_gamblers_mutex = Lock()
 
 class Gambler(Model):
     _table = "gamblers"
 
+    @prevent_concurrent(_gamblers_mutex)
     def __init__(self, account_id, bet_id):
         super().__init__(0)
         self._exist = False
@@ -14,7 +21,7 @@ class Gambler(Model):
             self._bet_amount, self._choice = cur.fetchone()
             self._exist = True
         except:
-            self.bet_amount = 0
+            self._bet_amount = 0
             self._choice = None
 
     @property
@@ -29,6 +36,7 @@ class Gambler(Model):
     def account_id(self):
         return self._account_id
 
+    @prevent_concurrent(_gamblers_mutex)
     def create(self, bet_amount, choice):
         self._connection.cursor().execute("INSERT INTO gamblers (account_id, bet_id, bet_amount, choice) VALUES (?, ?, ?, ?);", (self._account_id, self._bet_id, bet_amount, choice))
         self._bet_amount = bet_amount
@@ -36,6 +44,7 @@ class Gambler(Model):
         self._connection.commit()
 
     @staticmethod
+    @prevent_concurrent(_gamblers_mutex)
     def get_all_winner(bet_id, result):
         cursor = sqlite3.connect('../../db/franc.db').cursor()
         cursor.execute("SELECT * from gamblers WHERE bet_id=? AND choice=?", (bet_id, result))
